de69bbff24e532685ad6815a765acae767c10c0c
michelleph118 <98407349+michelleph118@users.noreply.github.com>
2025-05-21 00:55:27 +0200

    OPPSEE  Aufgabe

---
+++ src/Wecker.java
@@ -0,0 +1,38 @@
+public class Wecker {
+    private int tag;
+    private boolean istUrlaub;
+
+    public void weckerErstellen() {
+        String uhrzeit;
+        if (tag < 1) {
+            uhrzeit = "aus";
+        }
+        else if (tag <= 5) {
+            if (istUrlaub) {
+                uhrzeit = "10:00";
+            } else {
+                uhrzeit = "06:00";
+            }
+        } else {
+            if (istUrlaub) {
+                uhrzeit = "aus";
+            } else {
+                uhrzeit = "10:00";
+            }
+        }
+
+        if (uhrzeit.equals("aus")) {
+            System.out.println("Der Wecker ist ausgeschaltet.");
+        } else {
+            System.out.println("Der Wecker klingelt um " + uhrzeit);
+        }
+    }
+
+    public static void main(String[] args) {
+        Wecker wecker1 = new Wecker();
+        wecker1.tag = 5;
+        wecker1.istUrlaub = false;
+        wecker1.weckerErstellen();
+    }
+}
+
