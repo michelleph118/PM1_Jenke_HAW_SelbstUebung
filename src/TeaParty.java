4e70a9af539089eb45e6615f886f7c5f988f2071
michelleph118 <98407349+michelleph118@users.noreply.github.com>
2025-05-21 00:35:41 +0200

    OPPSEE TeaParty Aufgabe

---
+++ src/TeaParty.java
@@ -0,0 +1,56 @@
+
+    /**
+     * Die Klasse TeaParty bewertet die Qualität einer Tea Party
+     * basierend auf der Anzahl von Tee und Keksen.
+     *
+     * Die Bewertung erfolgt nach folgenden Regeln:
+     * - Wenn sowohl die Anzahl der Tees als auch die Anzahl der Kekse mindestens 5 beträgt:
+     *   - Falls die Anzahl des Tees mindestens doppelt so hoch ist wie die Anzahl der Kekse
+     *     oder umgekehrt, ist die Party "super".
+     *   - Andernfalls ist die Party "gut".
+     * - Falls eine der beiden Mengen weniger als 5 beträgt, ist die Party "schlecht".
+     */
+    public class TeaParty {
+        private String qualitaet;
+        private int anzahlTee;
+        private int anzahlKekse;
+
+        /**
+         * Bewertet die Qualität der Tea Party basierend auf den Mengen von Tee und Keksen
+         * und gibt das Ergebnis in der Konsole aus.
+         *
+         * Beispiele:
+         * teaParty(6, 8) → "Die Tea Party ist gut"
+         * teaParty(3, 8) → "Die Tea Party ist schlecht"
+         * teaParty(20, 5) → "Die Tea Party ist super"
+         */
+        public void teaParty() {
+            //TODO: Implementiere die Methode
+            if (anzahlTee >= 5 && anzahlKekse >= 5) {
+                if (anzahlTee >= 2 * anzahlKekse || anzahlKekse >= 2 * anzahlTee) {
+                    qualitaet = "super";
+                } else {
+                    qualitaet = "gut";
+                }
+            }
+            else
+            {
+                qualitaet = "schlecht";
+            }
+
+            System.out.println("Die Tea Party ist " + qualitaet);
+        }
+
+        public static void main(String[] args) {
+            TeaParty Party1 = new TeaParty();
+            TeaParty Geburtstag2 = new TeaParty();
+            Party1.anzahlTee = 10;
+            Party1.anzahlKekse = 5;
+            Geburtstag2.anzahlKekse = 5;
+            Geburtstag2.anzahlTee = 1;
+            Party1.teaParty();
+            Geburtstag2.teaParty();
+        }
+    }
+
+
